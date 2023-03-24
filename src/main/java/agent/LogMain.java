6eac345a368d42a8f6d09200635dc6eda62e3c2b
Zahra Hasan A Al Hilal <zalhilal1@sheffield.ac.uk>
2023-03-24 13:05:48 +0000

    added agent folder with files to extract trace

---
+++ src/main/java/agent/LogMain.java
@@ -0,0 +1,9 @@
+package agent;
+
+import java.lang.instrument.Instrumentation;
+
+public class LogMain {
+    public static void premain(String agentArguments, Instrumentation instrumentation) {
+        instrumentation.addTransformer(new Transformer(agentArguments));
+    }
+}
